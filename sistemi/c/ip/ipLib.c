64e3ec56998682aedbb11cdabaccf546f726a1db
gi0rg1s <sgiorgia.folloni@itis.pr.it>
2025-11-28 11:06:53 +0100

    ,hvvjhv

--- sistemi/c/ip/ipLib.c
+++ sistemi/c/ip/ipLib.c
@@ -1,11 +1,34 @@
 void parse_cli (int argc, char* argv[]){
     if(argc == 0) generate_ip(1);
     else if(argc >= 1){
-        char* str = argv[0];        //temporary var
-        if(str[0] == '-' && str[1] == 'n' && str[2] >= '0' && str[2] <= '9')  //parser for -n<number>
-            generate_ip(argv -'0');         //generate n ipv4
+        for (int i = 0; i < argc; i++){
+            char* str = argv[i];        //temporary var
+            if(str[0] == '-')  //parser for -n<number>
+
+            switch (str[1])
+            {
+            case 'n':
+                if(str[2] >= '0' && str[2] <= '9') generate_ip((int)str[2] - '0');
+                break;
+            
+            case 'c':
+                if(str[2] >= 'A' && str[2] <= 'E'){
+                    ip_t ip;
+
+                }
+                break;
+
+            case 'i':
+            //not implemented yet
+                break;
+
+            default:
+                break;
+            }
+        }
+            
     }
 }
-//uint8_t generate_ip (int n){
+uint8_t generate_ip (int n){
 
-//}
+}
