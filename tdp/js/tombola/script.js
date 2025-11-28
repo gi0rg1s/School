afdb604f22b74b6dbdcf5f262ad326f55b768617
gi0rg1s <sgiorgia.folloni@itis.pr.it>
2025-11-28 12:52:51 +0100

    kwjaebujewnh

--- tdp/js/tombola/script.js
+++ tdp/js/tombola/script.js
@@ -2,10 +2,12 @@ function table(){
     let tbl = '<table><tr>'
     for(let i = 0; i < 90; i++){
         if(i % 10 == 0 && i != 0) tbl += `</tr><tr>`
-        tbl += `<td>${i+1}</td>`
+        tbl += `<td class="${i+1}">${i+1}</td>`
     }
     tbl += `</table>`
     document.getElementById("tabella").innerHTML = tbl
 }
-
+function generate_random_number(){
+    let n = Math.floor(Math.random * 89) + 1
+}
 table()
