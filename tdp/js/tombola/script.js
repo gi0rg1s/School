5443788e9d499db96ad0248f87a3857877eeb00b
gi0rg1s <sgiorgia.folloni@itis.pr.it>
2025-11-30 18:31:46 +0100

    tombola

--- tdp/js/tombola/script.js
+++ tdp/js/tombola/script.js
@@ -1,13 +1,75 @@
-function table(){
-    let tbl = '<table><tr>'
-    for(let i = 0; i < 90; i++){
-        if(i % 10 == 0 && i != 0) tbl += `</tr><tr>`
-        tbl += `<td class="${i+1}">${i+1}</td>`
+let board = document.getElementById("tabella")
+let drawn_numbers = []
+let estrai = document.getElementById("estrai") 
+let reset = document.getElementById("reset")
+let intervalId; 
+let count = 0;
+let finalNumber;
+let currentCell;
+
+function init_table(){
+    board.innerHTML = ''
+    for(let i = 1; i <= 90; i++){
+        const cell = document.createElement("div")
+        cell.className = "cella"
+        cell.textContent = i
+        cell.id = `cell-${i}` 
+        board.appendChild(cell)
     }
-    tbl += `</table>`
-    document.getElementById("tabella").innerHTML = tbl
 }
+
 function generate_random_number(){
-    let n = Math.floor(Math.random * 89) + 1
+    if (drawn_numbers.length >= 90) {
+        alert("Tutti i numeri sono stati estratti");
+        return null
+    }
+    let n
+    do {
+        n = Math.floor(Math.random() * 90) + 1
+    } while (drawn_numbers.includes(n))
+    return n;
 }
-table()
+
+function print_animation(){
+    if (drawn_numbers.length >= 90) {
+        alert("Tutti i numeri sono stati estratti");
+        return;
+    }
+    count = 0;  
+    intervalId = setInterval(interval_function, 100); 
+}
+
+function interval_function(){
+    let tempN = Math.floor(Math.random() * 90) + 1;
+    document.getElementById("animation").innerText = tempN;
+    count++;
+    
+    if (count >= 15) {
+        clearInterval(intervalId);
+        
+        finalNumber = generate_random_number();
+        if (finalNumber === null) return;
+        
+        document.getElementById("animation").innerText = finalNumber;
+        drawn_numbers.push(finalNumber);
+        
+        currentCell = document.getElementById(`cell-${finalNumber}`);
+        currentCell.className = "cella-estratta";
+        
+        setTimeout(time_out, 1500);  
+    }
+}
+
+function time_out(){
+    currentCell.className = "cella cella-drawn";
+}
+
+function reset_table(){
+    drawn_numbers = []  
+    init_table()
+    document.getElementById("animation").innerText = ""
+}
+
+estrai.addEventListener('click', print_animation)
+reset.addEventListener('click', reset_table) 
+init_table()
