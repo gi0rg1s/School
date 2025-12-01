b6d74231fd4e9d74811e60b2985c856499f88d9e
gi0rg1s <sgiorgia.folloni@itis.pr.it>
2025-12-01 17:25:45 +0100

    fjiewhfije

---
+++ informatica/java/autonoleggio/Veicolo.java
@@ -0,0 +1,10 @@
+public abstract class Veicolo {
+    protected String targa;
+    protected int numeroMatricola;
+    protected String marca;
+    protected String modello;
+    protected double cilindrata;
+    protected int annoDiAcquisto;
+    protected double capacitaSerbatoio;
+
+}
