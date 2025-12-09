ab4469af5c4166e58c271a343be5b88cf4190a54
gi0rg1s <sgiorgia.folloni@itis.pr.it>
2025-12-09 18:03:33 +0100

    added comments and fixed a bit

--- tdp/js/tombola/script.js
+++ tdp/js/tombola/script.js
@@ -2,12 +2,13 @@ let board = document.getElementById("tabella")
 let drawn_numbers = []
 let estrai = document.getElementById("estrai") 
 let reset = document.getElementById("reset")
-let intervalId; 
-let count = 0;
-let finalNumber;
-let currentCell;
+let intervalId 
+let count = 0
+let finalNumber
+let currentCell
+let numbers = []
 
-function init_table(){
+function init_table(){                                              //create the table 
     board.innerHTML = ''
     for(let i = 1; i <= 90; i++){
         const cell = document.createElement("div")
@@ -19,14 +20,14 @@ function init_table(){
 }
 
 function generate_random_number(){
-    if (drawn_numbers.length >= 90) {
+    if (drawn_numbers.length >= 90) {                                           //extract a random number
         alert("Tutti i numeri sono stati estratti");
         return null
     }
     let n
     do {
         n = Math.floor(Math.random() * 90) + 1
-    } while (drawn_numbers.includes(n))
+    } while (drawn_numbers.includes(n))                                         //extract a new number while it is already extracted
     return n;
 }
 
@@ -36,7 +37,7 @@ function print_animation(){
         return;
     }
     count = 0;  
-    intervalId = setInterval(interval_function, 100); 
+    intervalId = setInterval(interval_function, 100);                           //interval for the animation
 }
 
 function interval_function(){
@@ -45,7 +46,7 @@ function interval_function(){
     count++;
     
     if (count >= 15) {
-        clearInterval(intervalId);
+        clearInterval(intervalId);                                              //generate 15 different numbers for the animation
         
         finalNumber = generate_random_number();
         if (finalNumber === null) return;
@@ -53,39 +54,38 @@ function interval_function(){
         document.getElementById("animation").innerText = finalNumber;
         drawn_numbers.push(finalNumber);
         
-        currentCell = document.getElementById(`cell-${finalNumber}`);
+        currentCell = document.getElementById(`cell-${finalNumber}`);          //change the class of the extract number to colorate it by red
         currentCell.className = "cella-estratta";
         
-        setTimeout(time_out, 1500);  
+        setTimeout(time_out, 1500);                                             //time sleeping
     }
 }
 
 function time_out(){
-    currentCell.className = "cella cella-drawn";
+    currentCell.className = "cella cella-drawn";                                //change again the class to be temporary drawn in red
 }
 
-function reset_table(){
+function reset_table(){                                                         //reset the table
     drawn_numbers = []  
     init_table()
     document.getElementById("animation").innerText = ""
 }
 
-function random_schedina(min, max){
-    let numbers = []
+function random_schedina(column, min, max){
     let number
     let row
     for(let i = 0; i < 2; i++){
         number = Math.floor(Math.random() * (max - min + 1)) + min
         row = Math.floor(Math.random() * 2)
-        if(!schedina[row] && !numbers.includes(number)){
-            schedina[row] = number 
+        if(!schedina[row][column] && !numbers.includes(number)){
+            schedina[row][column] = number 
             numbers.push(number)
         }
         else i--
     }
 }
 
-function init_schedine(){
+function init_schedine(){                                                           //initialize the schedina
     for(let n = 0; t < 5; t++){
         const schedina = document.createElement("div")
         schedina.className = `schedina${n+1}`
@@ -98,9 +98,11 @@ function init_schedine(){
                 document.getElementById(`schedina-${n+1}`).appendChild(cell)
             }
         }
-        for(let k = 0; k < 15; k++) random_schedina(n*10+1, (n*10)+10)
+        for(let k = 0; k < 9; k++) random_schedina(k, k*10+1, (k*10)+10)
+        numbers = []                                                                 //clear the array whit extract numbers for each schedina
     }
 }
+
 
 estrai.addEventListener('click', print_animation)
 reset.addEventListener('click', reset_table) 
