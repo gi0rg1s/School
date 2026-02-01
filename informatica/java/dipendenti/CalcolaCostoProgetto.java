b36a4d060c6aee2bf287dfec11612ac437f25850
gi0rg1s <sgiorgia.folloni@itis.pr.it>
2026-02-01 15:13:51 +0100

    fnished homework yaaas

--- informatica/java/dipendenti/CalcolaCostoProgetto.java
+++ informatica/java/dipendenti/CalcolaCostoProgetto.java
@@ -1,21 +1,13 @@
-import java.util.ArrayList;
-
 public class CalcolaCostoProgetto {
     public static void main(String[] args) {
-        ArrayList<Dipendente> dipendenti = new ArrayList<>();
-        dipendenti.add(new Dirigente("Mario", "Rossi", 2010));
-        dipendenti.add(new Funzionari("Luigi", "Verdi", 2015));
-        dipendenti.add(new TecnicoInformatico("Anna", "Bianchi", 2018, 1));
-        dipendenti.add(new TecnicoElettronico("Giovanni", "Neri", 2019, 0));
+        Progetto p = new Progetto(100);
+        p.aggiungiDipendente(new Dirigente("Mario", "Rossi", 2010));
+        p.aggiungiDipendente(new Funzionari("Luigi", "Verdi", 2015));
+        p.aggiungiDipendente(new TecnicoInformatico("Anna", "Bianchi", 2018, 1));
+        p.aggiungiDipendente(new TecnicoElettronico("Giovanni", "Neri", 2019, 0));
         int oreProgetto = 100;
-        double costoTotale = 0.0;
 
-        for (Dipendente d : dipendenti) {
-            double costo = d.costoPerProgetto(oreProgetto);
-            costoTotale += costo;
-            System.out.println("Costo per " + d.getNome() + " " + d.getCognome() + ": " + costo);
-        }
         System.out.println("-------------------------");
-        System.out.println("Costo totale progetto per tutti i dipendenti: " + costoTotale);
+        System.out.println("Costo totale progetto per tutti i dipendenti: " + p.calcolaCostoTotale(oreProgetto));
     }
 }
