7d5d22cc0385afba0c67a8e490b3e8f09fb37e83
gi0rg1s <sgiorgia.folloni@itis.pr.it>
2026-02-19 21:49:47 +0100

    still 15 points noooo

--- olinfo/abbassatutto/abbassatutto.c
+++ olinfo/abbassatutto/abbassatutto.c
@@ -7,12 +7,16 @@
 
 #define MAXN 100000
 
+int reverse_compare(const void *a, const void *b);
+void reverse_sort(int *arr, size_t n);
+
+
 int N, T, i, ris, test;
 int C[MAXN], P[MAXN];
 
 int main() {
     // decommenta le due righe seguenti se vuoi leggere/scrivere da file
-    freopen("abbassatutto_input_1.txt", "r", stdin);
+    freopen("abbassatutto_input_3.txt", "r", stdin);
     freopen("output.txt", "w", stdout);
 
     assert(1 == scanf("%d", &T));
@@ -30,14 +34,44 @@ int main() {
 
         // INSERISCI IL TUO CODICE QUI
 
-        for(int i = 0; i < N; i++){
-            ris += C[i] * P[i];
-        }
+            int temp[MAXN];
+            for (i = 0; i < N; i++) temp[i] = C[i];
+            
+            reverse_sort(temp, N);
+            int sum = 0;
+            if(temp[0] == temp[N - 1]){
+                for(int j = 0; j < N; j++) sum += P[j];
+                ris = sum * C[0];
+            }
 
+            else{
+                reverse_sort(P, N);
+                for (i = 0; i < N - 1; i++) {
+                    if(C[i] < C[i + 1]){
+                        P[i]  -= (P[i] - P[i + 1]) - 1;
+                        ris += (P[i] - P[i + 1] - 1) * C[i];
+                    }
+                    else{
+                        ris += P[i] * C[i];
+                        P[i] = 0;
+                    }
+                }
+                ris += P[N - 1] * C[N - 1];
+            }
 
         printf("Case #%d: ", test);
         printf("%d\n", ris);
     }
 
     return 0;
+}
+
+int reverse_compare(const void *a, const void *b) {
+    int x = *(const int *)a;
+    int y = *(const int *)b;
+    return (x < y) - (x > y);  // opposite of normal compare
+}
+
+void reverse_sort(int *arr, size_t n) {
+    qsort(arr, n, sizeof(int), reverse_compare);
 }
